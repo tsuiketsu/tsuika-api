5ae2050ab10aaf97e8f309ea12e71296ac992692
ImRayy <souradeep_roy@pm.me>
2025-05-11 19:32:49 +0530

    feat: add core API utility functions

---
+++ src/utils/api-error.ts
@@ -0,0 +1,27 @@
+class ApiError extends Error {
+  statusCode: number;
+  data: null;
+  success: false;
+  errors: Error[];
+
+  constructor(
+    statusCode: number,
+    message: "Something went wrong",
+    errors: [],
+    stack = "",
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
