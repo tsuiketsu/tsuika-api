181e60db85b7f03552f8791fd0615c0094a0ae2e
ImRayy <souradeep_roy@pm.me>
2025-11-04 00:41:30 +0530

    refactor(minio): generalize upload utilities to handle all object types

--- src/utils/minio.ts
+++ src/utils/minio.ts
@@ -1,18 +1,21 @@
+import { z } from "@hono/zod-openapi";
 import minioClient from "@/lib/minio";
 import { generatePublicId } from "./nanoid";
 
 // -----------------------------------------
-// UPLOAD IMAGE HANDLER
+// CREATE OBJECT
 // -----------------------------------------
-type ImageUploadArgs = {
+export const objectInsertSchema = z.discriminatedUnion("origin", [
+  z.object({ origin: z.literal("local"), fileUri: z.instanceof(File) }),
+  z.object({ origin: z.literal("remote"), fileUri: z.url() }),
+]);
+
+type CreateObjectArgs = {
   bucket: string;
   objectId?: string;
-} & (
-  | { origin: "local"; fileUri: File }
-  | { origin: "remote"; fileUri: string }
-);
+} & z.infer<typeof objectInsertSchema>;
 
-const generateUrl = (bucket: string, objectId: string) => {
+export const createObjectStoreURL = (bucket: string, objectId: string) => {
   return (
     process.env.S3_BUCKET_URL +
     `/api/v1/buckets/${bucket}/objects/download?` +
@@ -20,15 +23,25 @@ const generateUrl = (bucket: string, objectId: string) => {
   );
 };
 
-export type ImageBucketStoreResponse = {
+export type CreateObjectResponse = {
   fileId: string | null;
   url: string | null;
+  size: number | null;
+  name: string | null;
+  mimeType: string | null;
 };
 
-export async function storeImageToBucket(args: ImageUploadArgs): Promise<{
-  fileId: string | null;
-  url: string | null;
-}> {
+const defaultValue = {
+  fileId: null,
+  url: null,
+  size: null,
+  name: null,
+  mimeType: null,
+};
+
+export async function createBucketObject(
+  args: CreateObjectArgs,
+): Promise<CreateObjectResponse> {
   // Create bucket if not exists
   const exists = await minioClient.bucketExists(args.bucket);
 
@@ -43,7 +56,7 @@ export async function storeImageToBucket(args: ImageUploadArgs): Promise<{
       const response = await fetch(args.fileUri);
 
       if (!response.ok || !response.body) {
-        return { fileId: null, url: null };
+        return defaultValue;
       }
 
       // Create buffer
@@ -53,7 +66,7 @@ export async function storeImageToBucket(args: ImageUploadArgs): Promise<{
       const contentType = response.headers.get("content-type");
 
       const metaData = {
-        "Content-Type": contentType || "image/jpeg",
+        "Content-Type": contentType,
         "X-Amz-Meta-Project": "tsuika",
         "Cache-Control": "public, max-age=31536000",
       };
@@ -66,14 +79,20 @@ export async function storeImageToBucket(args: ImageUploadArgs): Promise<{
         metaData,
       ));
 
-      return { fileId: objectId, url: generateUrl(args.bucket, objectId) };
+      return {
+        fileId: objectId,
+        url: createObjectStoreURL(args.bucket, objectId),
+        mimeType: contentType,
+        name: args.fileUri,
+        size: buffer.length,
+      };
     } catch (error) {
       console.error(
         `Failed to upload file: "${args.fileUri}" to bucket ${args.bucket}`,
         error,
       );
 
-      return { fileId: null, url: null };
+      return defaultValue;
     }
   } else if (args.origin === "local") {
     try {
@@ -94,25 +113,31 @@ export async function storeImageToBucket(args: ImageUploadArgs): Promise<{
         metaData,
       ));
 
-      return { fileId: objectId, url: generateUrl(args.bucket, objectId) };
+      return {
+        fileId: objectId,
+        url: createObjectStoreURL(args.bucket, objectId),
+        size: buffer.length,
+        name: args.fileUri.name,
+        mimeType: args.fileUri.type,
+      };
     } catch (error) {
       console.error(
         `Failed to upload file: "${args.fileUri.name}" to bucket ${args.bucket}`,
         error,
       );
 
-      return { fileId: null, url: null };
+      return defaultValue;
     }
   }
 
-  return { fileId: null, url: null };
+  return defaultValue;
 }
 
 // -----------------------------------------
 // DELETE IMAGE HANDLER
 // -----------------------------------------
 
-export async function deleteImageFromBucket(bucket: string, fileId: string) {
+export async function deleteObjectFromBucket(bucket: string, fileId: string) {
   const exists = await minioClient.bucketExists(bucket);
 
   if (!exists) return;
@@ -123,5 +148,23 @@ export async function deleteImageFromBucket(bucket: string, fileId: string) {
     });
   } catch (error) {
     console.error(`Failed to delete file ${fileId}`, error);
+  }
+}
+
+// -----------------------------------------
+// DELETE IMAGES HANDLER
+// -----------------------------------------
+export async function deleteObjectsFromBucket(
+  bucket: string,
+  fileIds: string[],
+) {
+  const exists = await minioClient.bucketExists(bucket);
+
+  if (!exists) return;
+
+  try {
+    void (await minioClient.removeObjects(bucket, fileIds));
+  } catch (error) {
+    console.error(`Failed to delete file ${fileIds}`, error);
   }
 }
