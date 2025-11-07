7936b0a4eecd0ab8f9ebcfc253bd45fc93e807fb
ImRayy <souradeep_roy@pm.me>
2025-11-07 13:05:00 +0530

    chore: clean up env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,19 +1,13 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    PORT: string;
     DATABASE_URL: string;
     DOMAIN: string;
+    BASE_URL: string;
     FRONTEND_ORIGIN: string;
-    IMAGEKIT_URL_ENDPOINT: string;
-    IMAGEKIT_PUBLIC_KEY: string;
-    IMAGEKIT_PRIVATE_KEY: string;
     CORS_ORIGIN_HOPPSCOTCH: string;
-    CORS_ORIGIN_FRONTEND: string;
     CORS_ORIGIN_BROWSER_EXTENSION: string;
     RESEND_API_KEY: string;
     JWT_SECRET: string;
-    LINK_METADATA_API_URL: string;
-    LINK_METADATA_API_KEY: string;
     S3_BUCKET_USE_SSL: string;
     S3_BUCKET_API_PORT: number;
     S3_BUCKET_ENDPOINT: string;
